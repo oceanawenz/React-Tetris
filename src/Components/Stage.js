73b4c5b4d9558d417956b538e31cd4419124988d
Michael Wenz <michaelwenz@Michaels-iMac.local>
2019-08-20 12:37:23 -0700

    added custom hooks

--- src/Components/Stage.js
+++ src/Components/Stage.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Cell from './Cell';
+import { StyledStage } from './styles/StyledStage';
 
 const Stage = ({ stage }) => (
     //when mapping through the stage array we get the row and each row is an array that holds a cell and for each cell we will render the cell component
     //set type to cell[0] because we initially set the 
-    <div>
+    <StyledStage width={stage[0].length} height={stage.length}>
         {stage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]}/> ))}
-    </div>
+    </StyledStage>
 )
 
 export default Stage;
