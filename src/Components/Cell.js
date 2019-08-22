0db2a01a8909e5f7f8f0760a92c849333b17f2f5
Michael Wenz <michaelwenz@Michaels-iMac.local>
2019-08-21 17:18:55 -0700

    done

--- src/Components/Cell.js
+++ src/Components/Cell.js
@@ -3,7 +3,9 @@ import { StyledCell } from './styles/StyledCell';
 import { TETROMINOS } from '../tetrominos';
 
 const Cell = ({ type }) => (
-    <StyledCell type={type} color={TETROMINOS[type].color}/>
+    <StyledCell type={type} color={TETROMINOS[type].color}>{console.log('rerender')}</StyledCell>
 )
 
-export default Cell
+//React memo will make sure it memorizes the Cell component when the acutal cell rerenders and changes that the tetromino is in and not the rest
+
+export default React.memo(Cell)
--- src/hooks/useGameStatus.js
+++ src/hooks/useGameStatus.js
@@ -14,6 +14,14 @@ export const useGameStatus = (rowsCleared) => {
             //if we cleared 2 rows, we need to grab the value at index 1 in the line points array
             //level starts at 0
             setScore(prev => prev + linePoints[rowsCleared - 1] * (level + 1));
+            //keeps track of total rows cleared
+            setRows(prev => prev + rowsCleared);
         }
-    })
+    }, [level, linePoints, rowsCleared])
+
+    useEffect(() => {
+        calcScore();
+    }, [calcScore, rowsCleared, score]);
+
+    return [score, setScore, rows, setRows, level, setLevel];
 }
